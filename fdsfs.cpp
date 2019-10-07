46fd6f6d4ffbc3554dcaae6ccd133190cb9765f0
KappasTV <43045902+KappasTV@users.noreply.github.com>
2019-10-07 19:29:03 +0300

    Update fdsfs.cpp

--- fdsfs.cpp
+++ fdsfs.cpp
@@ -1,170 +1,176 @@
 #include <bits/stdc++.h>
-#include <Windows.h>
-#include <TlHelp32.h>
-#include <chrono>
-
-
-#define WINDOWS_TICK 10000000
-#define SEC_TO_UNIX_EPOCH 11644473600LL
-#define NUMBER_OF_PRIORITIES 8
-
-typedef long long ll;
-typedef long double ld;
-
-using namespace std;
-using namespace std::chrono;
-
-unsigned WindowsTickToUnixSeconds(ll windowsTicks)
-{
-    return (unsigned)(windowsTicks / WINDOWS_TICK - SEC_TO_UNIX_EPOCH);
-}
-
-int NUMBER_OF_PROCESSES;
-
-int main()
-{
-    STARTUPINFO startupinfo;
-    ZeroMemory(&startupinfo,sizeof(STARTUPINFO));
-    PROCESS_INFORMATION process_information = {nullptr};
-
-    printf("Input the number of the processes\n");
-    cin >> NUMBER_OF_PROCESSES;
-
-    auto start = high_resolution_clock::now();
-
-
-    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
-    if( hSnap == INVALID_HANDLE_VALUE )
-    {
-        return( FALSE );
-    }
-
-    HANDLE PROCESSES[NUMBER_OF_PROCESSES];
-    DWORD pids[NUMBER_OF_PROCESSES];
-    HANDLE THREADS[NUMBER_OF_PROCESSES];
-    DWORD tids[NUMBER_OF_PROCESSES];
-
-    DWORD PRIORITIES[NUMBER_OF_PRIORITIES] = {ABOVE_NORMAL_PRIORITY_CLASS, BELOW_NORMAL_PRIORITY_CLASS,
-                           HIGH_PRIORITY_CLASS, IDLE_PRIORITY_CLASS, NORMAL_PRIORITY_CLASS, PROCESS_MODE_BACKGROUND_BEGIN,
-                           PROCESS_MODE_BACKGROUND_END, REALTIME_PRIORITY_CLASS,
-                           };
-
-    for( int i = 0; i < NUMBER_OF_PROCESSES; i++)
-    {
-        BOOL process = CreateProcess(TEXT("C:\\Users\\Admin\\CLionProjects\\newAlgo\\cmake-build-debug\\labs0ss.exe"),
-                                     NULL, NULL,
-                                     NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &startupinfo, &process_information);
-        if (!process)
-        {
-            cout << "Something went wrong..." << endl;
-            return 1;
-        }
-        PROCESSES[i] = process_information.hProcess;
-        pids[i] = process_information.dwProcessId;
-        THREADS[i] = process_information.hThread;
-        tids[i] = process_information.dwThreadId;
-    }
-
-        int task,index;
-        bool looper = true;
-        while (looper)
-        {
-            printf("~~~|| Processes control menu ||~~~\n\n");
-            printf("1 - Stop the chosen process\n");
-            printf("2 - Start the chosen process\n");
-            printf("3 - Complete the chosen process\n");
-            printf("4 - Kill the chosen process\n");
-            printf("5 - Set priorities of processes\n");
-            printf("6 - Set priority of the chosen process\n");
-            printf("7 - Get the information about the chosen process\n");
-            printf("8 - Exit from the menu and check the time\n");
-            printf("9 - Exit from the program\n\n");
-            printf("~~~|| Processes control menu ||~~~\n\n");
-            printf("Select the command: ");
-            cin >> task;
-            switch (task){
-                case 1:
-                    printf("Stop the process. Select an id: ");
-                    cin >> index;
-                    SuspendThread(THREADS[index-1]);
-                    printf("Done!\n");
-                    break;
-                case 2:
-                    printf("Continue the process. Select an id: ");
-                    cin >> index;
-                    ResumeThread(THREADS[index-1]);
-                    if(ResumeThread(THREADS[index-1]) == DWORD(0)){
-                        printf("The chosen process hasn`t been suspended yet!");
-                    }else if(ResumeThread(THREADS[index-1]) == DWORD(1)){
-                        printf("The chosen process has already been resumed!");
-                    }
-                    else if(ResumeThread(THREADS[index-1]) > DWORD(1)){
-                        printf("The chosen process is still suspended!");
-                    }
-                    printf("Done!\n");
-                    break;
-                case 3:
-                    printf("Complete the process. Select an id: ");
-                    cin >> index;
-                    DWORD EXIT;
-                    TerminateThread(THREADS[index-1],EXIT);
-                    break;
-                case 4:
-                    printf("Kill the process. Select an id: ");
-                    cin >> index;
-                    TerminateProcess(PROCESSES[index-1],UINT (0));
-                    printf("Done. The process number %d was terminated\n",index);
-                    break;
-                case 5:
-                    printf("Changing priorities of %d processes....\n", NUMBER_OF_PROCESSES);
-                    for(int i = 0; i < NUMBER_OF_PROCESSES; i++)
-                    {
-                        if((i+1)%2 == 0){
-                            SetPriorityClass(PROCESSES[i],NORMAL_PRIORITY_CLASS);
-                        } else{
-                            SetPriorityClass(PROCESSES[i],REALTIME_PRIORITY_CLASS);
-                        }
-                    }
-                    printf("Done!\n");
-                    break;
-                case 6:
-                    printf("Select a process: ");
-                    cin >> index;
-                    int priority;
-                    printf("Select a priority: ");
-                    cin >> priority;
-                    SetPriorityClass(PROCESSES[index-1],PRIORITIES[priority-1]);
-                    printf("Done!\n");
-                    if(priority > NUMBER_OF_PRIORITIES){
-                        printf("Wrong priority input!\n");
-                    }
-                    break;
-                case 7:
-                    printf("Select an index to get more information: ");
-                    cin >> index;
-                    cout << PROCESSES[index-1] << " | " << pids[index-1] << " | " << GetPriorityClass(PROCESSES[index-1]) <<
-                    " | " << tids[index-1] << endl;
-                    printf("Done!\n");
-                    break;
-                case 8:
-                    looper = false;
-                    break;
-                case 9:
-                    printf("Bye!");
-                    for(int i = 0; i < NUMBER_OF_PROCESSES; i++)
-                    {
-                        TerminateProcess(PROCESSES[i],UINT(0));
-                    }
-                    return 1;
-                default:
-                    printf("Wrong input!\n");
-            }
-        }
-
-        auto stop = high_resolution_clock::now();
-
-        auto duration = duration_cast<microseconds>(stop - start);
-
-        cout << duration.count() / pow(10, 6) << endl;
-    return 0;
-}
+//#include <windows.h>
+//#include <chrono>
+//#define pb push_back
+//using namespace std::chrono;
+//using namespace std;
+//typedef long long ll;
+//int NUMBER_OF_THREADS;
+//CRITICAL_SECTION critical_section;
+//struct FUNC_ARGS{
+//    int left;
+//    int right;
+//    vector<int>&array;
+//};
+//vector<ll> temporary;
+//vector<vector<ll>>multi;
+//void ShellSort(LPVOID p_args){
+//    EnterCriticalSection(&critical_section);
+//    auto *args = (FUNC_ARGS *) (p_args);
+//    int size = args->right - args->left + 1;
+//    for (int j = args->left; j <= args->right; j++) {
+//        temporary.pb(args->array[j]);
+//    }
+//    for (int gap = size / 2; gap > 0; gap /= 2) {
+//        for (int j = gap; j < size + 1; j++) {
+//            ll temp = temporary[j];
+//            int k;
+//            for (k = j; k >= gap && temporary[k - gap] > temp; k -= gap) {
+//                temporary[k] = temporary[k - gap];
+//            }
+//            temporary[k] = temp;
+//        }
+//    }
+//    LeaveCriticalSection(&critical_section);
+//}
+//int main()
+//{
+//    printf("\nInput number of threads\n");
+//    cin >> NUMBER_OF_THREADS;
+//    printf("\nInput number of elements for each part\n");
+//    int num;
+//    cin >> num;
+//    int NUMBER_OF_ELEMENTS = num * NUMBER_OF_THREADS;
+//    vector<int> array_for_sort(static_cast<unsigned int>(NUMBER_OF_ELEMENTS));
+//    auto start = high_resolution_clock::now();
+//    printf("\nRandomization of the array\n");
+//    for(int i = 0; i < NUMBER_OF_ELEMENTS; i++){
+//        array_for_sort[i] = rand()%10000;
+//        cout << array_for_sort[i] << " ";
+//    }
+//    cout << endl;
+//    cout << endl;
+//    HANDLE hThreads[NUMBER_OF_THREADS];
+//    DWORD dwThread_ids[NUMBER_OF_THREADS];
+//    int PRIORITIES[9] = {THREAD_MODE_BACKGROUND_BEGIN,THREAD_MODE_BACKGROUND_END,THREAD_PRIORITY_ABOVE_NORMAL
+//            ,THREAD_PRIORITY_BELOW_NORMAL,THREAD_PRIORITY_HIGHEST,THREAD_PRIORITY_IDLE,
+//                         THREAD_PRIORITY_LOWEST,THREAD_PRIORITY_NORMAL,THREAD_PRIORITY_TIME_CRITICAL};
+//    int left = 1,right = num;
+//    InitializeCriticalSection(&critical_section);
+//    for(int i = 0; i < NUMBER_OF_THREADS; i++){
+//        FUNC_ARGS args = {left-1,right-1,array_for_sort};
+//        hThreads[i] = CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)ShellSort,&args,CREATE_SUSPENDED,&dwThread_ids[i]);
+//        left = right+1;
+//        right += num;
+//    }
+//    printf("\nThread`s creation completed\n");
+//    bool looper = true;
+//    int task;
+//    int index;bool inner = true;
+//    int priority;
+//    while (looper){
+//        printf("\n~~~|| Threads control menu ||~~~\n");
+//        printf("1 - Run all the threads\n");
+//        printf("2 - Print the results\n");
+//        printf("2 - Set a priority of the chosen thread\n");
+//        printf("3 - Exit from the menu and check the time\n");
+//        printf("~~~|| Threads control menu ||~~~\n\n");
+//        printf("Select the command : ");
+//        cin >> task;
+//        switch (task){
+//            case 1:
+//                for(int i = 0; i < NUMBER_OF_THREADS; i++){
+//                    ResumeThread(hThreads[i]);
+//                    WaitForSingleObject(hThreads[i], INFINITE);
+//                    multi.push_back(temporary);
+//                }
+//                break;
+//            case 2:
+//                for(int i = 0; i < NUMBER_OF_THREADS; i++) {
+//                    printf("\n||| Part # %d |||\n", i + 1);
+//                    for(int j = 0; j <= i; j++ ){
+//                        printf("\n~~~");
+//                        sort(multi[j].begin(),multi[j].end());
+//                        for(auto &item : multi[j]){
+//                            cout << item << " ";
+//                        }
+//                        printf("~~~");
+//                        printf("\n");
+//                    }
+//                }
+//                break;
+//            case 3:
+//                printf("Input the index : ");
+//                cin >> index;
+//                while (inner) {
+//                    printf("~~~|| Priorities menu ||~~~\n\n");
+//                    printf("1 - THREAD_MODE_BACKGROUND_BEGIN\n");
+//                    printf("2 - THREAD_MODE_BACKGROUND_END\n");
+//                    printf("3 - THREAD_PRIORITY_ABOVE_NORMAL\n");
+//                    printf("4 - THREAD_PRIORITY_BELOW_NORMAL\n");
+//                    printf("5 - THREAD_PRIORITY_HIGHEST\n");
+//                    printf("6 - THREAD_PRIORITY_IDLE\n");
+//                    printf("7 - THREAD_PRIORITY_LOWEST\n");
+//                    printf("8 - THREAD_PRIORITY_NORMAL\n");
+//                    printf("9 - THREAD_PRIORITY_TIME_CRITICAL\n");
+//                    printf("10 - Exit\n");
+//                    printf("~~~|| Priorities menu ||~~~\n\n");
+//                    printf("Choose the priority : ");
+//                    cin >> priority;
+//                    switch (priority){
+//                        case 1:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 2:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 3:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 4:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 5:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 6:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 7:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 8:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 9:
+//                            SetThreadPriority(hThreads[index-1],PRIORITIES[priority-1]);
+//                            inner = false;
+//                            break;
+//                        case 10:
+//                            inner = false;
+//                            break;
+//                        default:
+//                            printf("Wrong input!");
+//                    }
+//                }
+//                break;
+//            case 4:
+//                looper = false;
+//                break;
+//            default:
+//                printf("Wrong input!");
+//        }
+//    }
+//    auto stop = high_resolution_clock::now();
+//    auto duration = duration_cast<microseconds>(stop - start);
+//    printf("Execution time - %lf seconds\n",duration.count()/pow(10,6));
+//    return 0;
+//}
